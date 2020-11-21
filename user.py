9cb0701b7f5ce0dcd3ebeba1d7970734f135c258
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-21 19:14:27 +0300

    create new function that initialize our password and user name

--- user.py
+++ user.py
@@ -1,4 +1,20 @@
 from credentials import Credentials
 
 class User(Credentials):
-    
+
+    def __init__(self):
+        super().__init__()
+        self.username =None
+        self.password =None
+        self.isLoggedIn = False
+        if self.isLoggedIn ==False:
+            user_input = int(input('Reply with 1 to login or 2 to signup(1/2): '))
+            username = input('username:')
+            password = input('password:')
+            if user_input == 1:
+                self.sign_in(username,password)
+            self.create_new_user(username,password)  
+
+            
+              
+
