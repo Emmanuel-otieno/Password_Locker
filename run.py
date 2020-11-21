e9a18c6ee55d947faea72b7aa328a43d499fc53f
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-21 21:03:55 +0300

    add methed

--- run.py
+++ run.py
@@ -2,4 +2,11 @@
 from user import User
 
 def main():
-    
+    while True:
+        print ("Welcom to password locker")
+        print("\n")
+        print("select a short code to navigate through:to login'in',to exit'ex': to creat a new user 'nw'")
+        short_code = input().lower()
+        print("\n")
+
+        
