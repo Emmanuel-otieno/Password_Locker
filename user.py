44df6016e3fe269ea88ed5f2657fa319714f85f8
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-21 15:46:06 +0300

    add a  created a function that craete password

--- user.py
+++ user.py
@@ -6,8 +6,14 @@ class Credetials:
     def new_account(self, accont_name,username,password=None):
         if password is None:
             user_input = int(input('specify the length of password needed: ')) 
-            password = self.generate_password(user_input) 
+            password = self.create_password(user_input) 
             account = ('account_naame':account_name,'username':username,'password':password)
             self.account.append(account)
             print("your account is created sucessfully")
             return account
+
+
+    def  create_password(self,length=6):
+        alphabet = ascli_letters + digits + punctuation
+        return "".join( choice(alphabet) for i in range(length))
+        
