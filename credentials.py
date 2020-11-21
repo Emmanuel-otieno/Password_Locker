7b2e93e5c606dbf4b40763d439815082985e068f
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-21 20:53:02 +0300

    add a new file

--- credentials.py
+++ credentials.py
@@ -1,13 +1,13 @@
 
 class Credentials:
     def __init__(self):
-        self.account =[]
+        self.accounts =[]
 
     def new_account(self, accont_name,username,password=None):
         if password is None:
             user_input = int(input('specify the length of password needed: ')) 
             password = self.create_password(user_input) 
-            account = ('account_naame':account_name,'username':username,'password':password)
+            account = ('account_name':account_name'username':username,'password':password)
             self.account.append(account)
             print("your account is created sucessfully")
             return account
