b5f6c02e4c23402bdc31c2cff5460d548297a78a
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-21 15:37:58 +0300

    add a  created a function that craete an account

--- user.py
+++ user.py
@@ -2,3 +2,12 @@
 class Credetials:
     def __init__(self):
         self.account =[]
+
+    def new_account(self, accont_name,username,password=None):
+        if password is None:
+            user_input = int(input('specify the length of password needed: ')) 
+            password = self.generate_password(user_input) 
+            account = ('account_naame':account_name,'username':username,'password':password)
+            self.account.append(account)
+            print("your account is created sucessfully")
+            return account
