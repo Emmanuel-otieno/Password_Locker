e71ab8fdc5b175d80c4b93cbd143f15971866686
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-23 11:22:47 +0300

    remove desplay account method

--- user.py
+++ user.py
@@ -1,67 +1,50 @@
-from credentials import Credentials
+class User:
+    """
+    A class that generates new users 
+    """
+    account_list = []
 
-class User(Credentials):
-
-    def __init__(self):
-        super().__init__()
-        self.username =None
-        self.password =None
-        self.isLoggedIn = False
-        if self.isLoggedIn ==False:
-            user_input = int(input('Reply with 1 to login or 2 to signup(1/2): '))
-            username = input('username:')
-            password = input('password:')
-            if user_input == 1:
-                self.sign_in(username,password)
-            self.create_new_user(username,password)  
-
-
-    def create_new_user(self,username,password):
-        self.username = username
+    def __init__(self,user_name,account_name,password,email):
+        self.user_name = user_name
+        self.account_name = account_name
         self.password = password
-        print("Account created successfully")
-        self.isLoggedIn = True
+        self.email = email
 
+    def save_account(self):
+        """
+        A method saves account objects into account_list
+        """  
+        User.account_list.append(self)  
 
-    def view_accounts(self):
-        for ac in self.accounts:
-            print(ac)
-        return self.accounts  
+    def delete_account(self):
+        """
+        A method that deletes saved accounts from the account list
+        """
+        
+        User.account_list.remove(self) 
+    @classmethod
+    def find_by_name(cls,name):
+        for account in cls.account_list:
+            if account.account_name == name:
+                return account  
 
+    @classmethod
+    def account_exist(cls,name):
+        '''
+        Method that checks if a account exists from the account list.
+        Args:
+            name: Acc name to search if it exists
+        Returns :
+            Boolean: True or false depending if the account exists
+        '''
+        for account in cls.account_list:
+            if account.password == name:
+                    return account  
 
-    def sign_in(self,username,password):
-        if username == self.username and password == self.password:
-            self.isLoggedIn = True
-            print('success')
-            return "Login successfully"
-        else:
-            print('Login failed')  
-            return "incorrect username"
-
-
-    def log_out(self):
-        self.isLoggedIn = False
-        return 'Logout successfully'
-
-
-
-c = user()
-c.create_new_user('test_user','password')
-print(c.create_password(30))
-print(c.new_account('LI','linkedin','p@$$w0rd'))
-print(c.new_account('fb','facebook', '@n0ther|p@$$w0rd'))
-print("=====================================================")
-print(c.get_account_details('LI'))
-print(c.delete_account('erty'))
-print(c.accounts)
-print('==============View Account============')
-print(c.view_accounts())
-
-print('====================Log Out======================')
-print(c.log_out())
-print('====================View Accounts======================')
-
-print(c.view_accounts())
-
-
-
+        return False
+    @classmethod
+    def display_accounts(cls):
+        '''
+        method that returns the account list
+        '''
+        return cls.account_list        
